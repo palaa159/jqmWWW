69edc1440e9bf2f6b5f8bfed51df563f036656f5
Apon Palanuwech <palaa159@newschool.edu>
2013-08-24 00:30:04 -0400

    1

---
+++ js/detailview.js
@@ -0,0 +1,39 @@
+console.log('detailview loaded');
+var pageBefore;
+$(document).delegate('#detailView', 'pagebeforeshow', function(event, data) {
+    pageBefore = data.prevPage.attr('id');
+});
+
+$(document).delegate("#detailView", "pageshow", function() {
+	// for vimeo player
+	var iframe = $('#vimeo-player')[0];
+	var player = $f(iframe);
+	var url = 'http://player.vimeo.com/video/' + fetchedData[currData].url + '?title=0&portrait=0&byline=0&api=1&autoplay=0';
+	$('#backToBefore').click(function() {
+		console.log('back');
+		player.api('pause');
+		// clear detailview
+		
+		$('#vimeo-player').attr('src', '');
+		$('#detailNodeTitle').html('');
+		$('#detailNodeAddress').html('');
+		$('#detailNodeDesc').html('');
+		$('#detailSeqNum').html('');
+		$.mobile.navigate('#' + pageBefore);
+	});
+	console.log('detailview refreshed');
+	var WIDTH = sW - 32;
+	$('#detailNodeTitle').html(fetchedData[currData].name);
+	$('#detailNodeAddress').html(fetchedData[currData].address);
+	$('#vimeo-player').attr({
+		'width': WIDTH,
+		'height': WIDTH*9/16,
+		'src': url
+	});
+	// Auto play
+	// setTimeout(function() {
+	// 	player.api('play');
+	// },1000);
+	$('#detailSeqNum').html('[' + fetchedData[currData].ID + '/' + fetchedData.length + ']');
+	$('#detailNodeDesc').html(fetchedData[currData].desc);
+});
---
+++ js/mapinit.js
@@ -0,0 +1,109 @@
+console.log('mapinit loaded');
+var map, myPos, newLatLng;
+
+$(document).delegate("#mapView", "pageinit", function() {
+	$('#locatingBox').css({
+		'left': sW / 2 - $('#locatingBox').width() / 2
+	});
+	init();
+	detectUserLocation();
+	$('#map').css({
+		'height': sH - 42
+	});
+	$('#clientLocate').click(function() {
+		mapHalfOpacity();
+		$('#locatingBox').fadeIn();
+		clientLocate();
+	});
+});
+
+function init() {
+	// initiate leaflet map
+	map = new L.Map('map', {
+		center: [40.732161, -73.97832],
+		minZoom: 14,
+		zoom: 16,
+		zoomControl: false
+	});
+	// set map bound
+
+	// map.setMaxBounds([
+	// 	[40.726316, -73.994808],
+	// 	[40.742445, -73.960047]
+	// ]);
+
+	// user
+
+	L.tileLayer('http://a.tiles.mapbox.com/v3/michaelisanerd.map-2s73eo1z/{z}/{x}/{y}.png').addTo(map);
+	myPos = new L.circleMarker([0, 0], {
+		stroke: false,
+		fillColor: '#3399ff',
+		fillOpacity: 1,
+		radius: 8
+	}).bindPopup('This is YOU').addTo(map);
+
+	// nodes
+	// query data
+	var nodes = [];
+	$.each(fetchedData, function(i, v) {
+		nodes[i] = L.marker(v.geoData)
+		.addTo(map)
+		.bindPopup('<a onclick="setCurrData(' + i + ');" href="#detailView"><div class="contextualView">' + v.address +'<br>' + v.author +'</div></a>');
+	});
+	map.invalidateSize();
+}
+
+function setCurrData(a) {
+	currData = a;
+}
+
+function detectUserLocation() {
+	if (navigator.geolocation) {
+		var timeoutVal = 10 * 1000 * 1000;
+		navigator.geolocation.watchPosition(
+		mapToPosition, alertError, {
+			enableHighAccuracy: true,
+			timeout: timeoutVal,
+			maximumAge: 0
+		});
+	} else {
+		alert("Geolocation is not supported by this browser");
+	}
+
+	function alertError(error) {
+		var errors = {
+			1: 'Permission denied',
+			2: 'Position unavailable',
+			3: 'Request timeout'
+		};
+		alert("Error: " + errors[error.code]);
+	}
+}
+
+function mapToPosition(position) {
+	lng = position.coords.longitude;
+	lat = position.coords.latitude;
+	newLatLng = new L.LatLng(lat, lng);
+	myPos.setLatLng(newLatLng);
+	$('#locatingBox').fadeOut();
+	mapFullOpacity();
+	map.invalidateSize();
+}
+
+function clientLocate() {
+	map.setView(newLatLng, 16);
+	$('#locatingBox').fadeOut();
+	mapFullOpacity();
+}
+
+function mapHalfOpacity() {
+	$('#map').animate({
+		'opacity': 0.5
+	});
+}
+
+function mapFullOpacity() {
+	$('#map').animate({
+		'opacity': 1
+	});
+}
